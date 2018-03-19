2b1bc8b3b1403b9b3c57521737aec2ccd477d668
冯天祥 <751728440@qq.com>
2018-03-20 01:46:24 +0800

    2018年3月20日01:46:21

--- js/annotate.js
+++ js/annotate.js
@@ -1,7 +1,9 @@
 // 选中并传递到popup
 var text;
+
 function getString(n) {
   text = rangy.getSelection().toString();
+  console.log(text);
   var string = 'selectedtxt' + n;
   var selectedtxt = document.getElementById(string);
   selectedtxt.innerText = text;
@@ -9,9 +11,10 @@ function getString(n) {
 
 //将popu中的文字传递到侧边栏
 function panel(m) {
-  var string = 'note'+m;
-  var textarea = document.getElementById('string').textarea.value;
-  $$('#ancontend').append('<div><p>原文：</p><p>' + text + '</p><p>我的批注</p><p>'+textarea+'</P></div>')
+  var string = 'note' + m;
+  var textarea = document.getElementById(string).value;
+  $$('#ancontent').append('<div><p>原文：' + text + '</p><p>我的批注:' + textarea + '</p></div>')
+  document.getElementById(string).value = '';
 }
 
 //获取选中文字的位置
@@ -37,17 +40,16 @@ function getLocation() {
     i++;
   }
   i = (i + 1) / 2;
-  // alert(i);
-  // alert(rangeObject.startOffset);
-  // alert(rangeObject.endOffset);
-  // alert(getString());
+  alert(i);
+  alert(rangeObject.startOffset);
+  alert(rangeObject.endOffset);
+  alert(getString());
 }
 
 //添加批注样式
 function anPaint(bton) {
   rangy.init();
   var classApplierModule = rangy.modules.ClassApplier;
-
   if (rangy.supported && classApplierModule && classApplierModule.supported) {
     switch (bton) {
       case 0:
@@ -74,57 +76,56 @@ function anPaint(bton) {
   }
 }
 
-//传递选中文字
-function trans() {
-  alert(getString());
-  $$('#selectedTxt0').text(getString());
-}
-
 // 底部工具栏按钮事件
 var button0 = document.getElementById("button0");
 button0.ontouchstart = function () {
-  // getLocation();
   getString(0);
-
 }
-// $$('botton0').on('click',function(){
-//   getString();
-// })
+
 var botton1 = document.getElementById("button1");
 button1.ontouchstart = function () {
-  getLocation();
+  getString(1);
 }
+
 var botton2 = document.getElementById("button2");
 button2.ontouchstart = function () {
-  getLocation();
+  getString(2);
 }
+
 var botton3 = document.getElementById("button3");
 button3.ontouchstart = function () {
-  getLocation();
+  getString(3);
 }
+
 var botton4 = document.getElementById("button4");
 button4.ontouchstart = function () {
-  getLocation();
+  getString(4);
 }
 
 //添加批注按钮点击事件
 var add0 = document.getElementById("add0");
 add0.ontouchstart = function () {
   anPaint(0);
+  panel(0);
 }
+
 var add1 = document.getElementById("add1");
 add1.ontouchstart = function () {
   anPaint(1);
+  panel(1)
 }
 var add2 = document.getElementById("add2");
 add2.ontouchstart = function () {
   anPaint(2);
+  panel(2);
 }
 var add3 = document.getElementById("add3");
 add3.ontouchstart = function () {
   anPaint(3);
+  panel(3);
 }
 var add4 = document.getElementById("add4");
 add4.ontouchstart = function () {
   anPaint(4);
+  panel(4);
 }
