210c7eb8d968d5fa55b1cc2291047ba0ef447779
冯天祥 <751728440@qq.com>
2018-05-11 20:45:54 +0800

    2018年5月11日20:45:50

--- js/framework7.js
+++ js/framework7.js
@@ -17589,10 +17589,10 @@ var Calendar = {
   params: {
     calendar: {
       // Calendar settings
-      monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+      monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+      monthNamesShort: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+      dayNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+      dayNamesShort: ['日', '一', '二', '三', '四', '五', '六'],
       firstDay: 1, // First day of the week, Monday
       weekendDays: [0, 6], // Sunday and Saturday
       multiple: false,
